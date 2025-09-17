55ab43c666cfdd86915f7b9f7972141ff9ef0d76
tiarakoliqi <tiarakoliqi@gmail.com>
2025-09-17 17:46:21 +0200

    Add files via upload

---
+++ Leason30/js/main.js
@@ -0,0 +1,9 @@
+console.log(Math.random()) //floor
+//integer
+console.log (Math.floor(Math.random()*5))
+
+var student =["John" , "drin"]
+var [s1,s2] = student;
+
+console.log(s1)
+console.log(s2)
---
+++ Leason30/lesson30/js/main.js
@@ -0,0 +1,21 @@
+var x =5;
+
+if (x>2) { //true
+    console.log("x is greater than 2 ");
+} else {
+    console.log ("x is less than 2");
+}
+
+var input =document.getElementById("input_id")
+var button = document.getElementById("bnt_id")
+var text =document.getElementById( "text_id")
+
+button.onclick =function(){
+    text.innerHTML = input.value;
+}
+
+var input =document.getElementById('input_id')
+var input =document.getElementById|('bnt_id')
+button.onclick =function(){
+    
+}

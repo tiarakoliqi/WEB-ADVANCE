ced80c75ebb58ab793cc072a636df42fb1f3b411
tiarakoliqi <tiarakoliqi@gmail.com>
2025-10-06 17:45:02 +0200

    Add files via upload

---
+++ Day35/js/menu/main.js
@@ -0,0 +1,8 @@
+funksion openMenu() {
+    var menu = document.getElementById("myLinks");
+    if(menu.style.display === 'block') {
+        menu.style.display = 'none';
+    }else {
+        menu.style.display = 'block';
+    }
+}
